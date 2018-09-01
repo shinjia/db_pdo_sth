f2eaa56b9a598bde761e5e9403d17ca478763288
Shinjia <shinjia168@gmail.com>
2018-09-01 14:22:04 +0800

    全部列表程式

--- list_all.php
+++ list_all.php
@@ -1,81 +1,74 @@
 <?php
 include 'config.php';
+include 'utility.php';
 
 // 連接資料庫
-$link = db_open();
+$pdo = db_open();
 
-
+// 寫出 SQL 語法
 $sqlstr = "SELECT * FROM person ";
 
-$result = mysqli_query($link, $sqlstr);
+$sth = $pdo->prepare($sqlstr);
 
-$total_rec = mysqli_num_rows($result);
-
-$data = '';
-while($row=mysqli_fetch_array($result))
+// 執行SQL及處理結果
+if($sth->execute())
 {
-   $uid      = $row['uid'];
-   $usercode = $row['usercode'];
-   $username = $row['username'];
-   $address  = $row['address'];
-   $birthday = $row['birthday'];
-   $height   = $row['height'];
-   $weight   = $row['weight'];
-   $remark   = $row['remark'];
+   // 成功執行 query 指令
+   $total_rec = $sth->rowCount();
+   $data = '';
+   while($row = $sth->fetch(PDO::FETCH_ASSOC))
+   {
+      $uid = $row['uid'];
+      $usercode = convert_to_html($row['usercode']);
+      $username = convert_to_html($row['username']);
+      $address  = convert_to_html($row['address']);
+      $birthday = convert_to_html($row['birthday']);
+      $height   = convert_to_html($row['height']);
+      $weight   = convert_to_html($row['weight']);
+      $remark   = convert_to_html($row['remark']);
+    
+      $data .= <<< HEREDOC
+       <tr>
+          <td>{$uid}</td>
+          <td>{$usercode}</td>
+          <td>{$username}</td>
+          <td>{$address}</td>
+          <td>{$birthday}</td>
+          <td>{$height}</td>
+          <td>{$weight}</td>
+          <td>{$remark}</td>
+          <td><a href="display.php?uid={$uid}">詳細</a></td>
+          <td><a href="edit.php?uid={$uid}">修改</a></td>
+          <td><a href="delete.php?uid={$uid}" onClick="return confirm('確定要刪除嗎？');">刪除</a></td>
+       </tr>
+HEREDOC;
+   }
    
-   // 依需要調整顯示內容
-   $str_birthday = date('Y-m-d', strtotime($birthday));
-
-   $data .= <<< HEREDOC
-   <tr>
-     <td>{$uid}</td>
-     <td>{$usercode}</td>
-     <td>{$username}</td>
-     <td>{$address}</td>
-     <td>{$str_birthday}</td>
-     <td>{$height}</td>
-     <td>{$weight}</td>
-     <td>{$remark}</td>
-     <td><a href="display.php?uid={$uid}">詳細</a></td>
-     <td><a href="edit.php?uid={$uid}">修改</a></td>
-     <td><a href="delete.php?uid={$uid}" onclick="return confirm('確定要刪除嗎？');">刪除</a></td>
-   </tr>
+   $html = <<< HEREDOC
+   <h2 align="center">共有 {$total_rec} 筆記錄</h2>
+   <table border="1" align="center">
+      <tr>
+         <th>序號</th>
+         <th>代碼</th>
+         <th>姓名</th>
+         <th>地址</th>
+         <th>生日</th>
+         <th>身高</th>
+         <th>體重</th>
+         <th>備註</th>
+         <th colspan="3" align="center"><a href="add.php">新增記錄</a></th>
+      </tr>
+      {$data}
+   </table>
 HEREDOC;
 }
+else
+{
+   // 無法執行 query 指令時
+   $html = error_message('list_all');
+}
 
-db_close($link);
 
-
-
-$html = <<< HEREDOC
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>基本資料庫系統</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-<p><a href="index.php">回首頁</a></p>
-<h2 align="center" align="center">共有 {$total_rec} 筆記錄</h2>
-<table border="1" align="center">
-   <tr>
-      <th>序號</th>
-      <th>代碼</th>
-      <th>姓名</th>
-      <th>地址</th>
-      <th>生日</th>
-      <th>身高</th>
-      <th>體重</th>
-      <th>備註</th>
-      <td colspan="3" align="center">操作 [<a href="add.php">新增記錄</a>]</td>
-   </tr>
-{$data}
-</table>
-
-</body>
-</html>
-HEREDOC;
-
-echo $html;
+include 'pagemake.php';
+pagemake($html, '');
 ?>
